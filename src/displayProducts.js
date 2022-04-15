ba9ecbc051dea6055bd6dda6e422be132a615939
Armand WADJI <armand_wadji@yahoo.fr>
2022-04-15 12:28:32 +0200

    featured page

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -1,5 +1,36 @@
-import { formatPrice } from './utils.js';
-import { addToCart } from './cart/setupCart.js';
-const display = () => {};
+import { formatPrice } from "./utils.js";
+import { addToCart } from "./cart/setupCart.js";
+const display = (data) => {
+  const products = data
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url } = product.fields.image[0];
+
+      return `
+        <article class="product">
+            <div class="product-container">
+            <img src=${url} alt="image of ${title}" class="product-img img" />
+            <div class="product-icons">
+                <a href="./product.html?id=${id}" class="product-icon">
+                    <i class="fas fa-search"></i>
+                </a>
+                <button class="product-cart-btn product-icon" data-id=${id}>
+                    <i class="fas fa-shopping-cart"></i>
+                </button>
+            </div>
+            </div>
+            <footer>
+            <p class="product-name">${title}</p>
+            <h4 class="product-price">$${price}</h4>
+            </footer>
+        </article>
+    
+    `;
+    })
+    .join("");
+
+  return products;
+};
 
 export default display;
--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,13 @@
-import { allProductsUrl } from './utils.js';
+import { allProductsUrl } from "./utils.js";
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+  try {
+    const response = await fetch(allProductsUrl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    document.body.innerHTML = `<h1 class="filter-error">error loading...</h1>`;
+  }
+};
 
 export default fetchProducts;
