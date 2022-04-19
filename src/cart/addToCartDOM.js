54e754d5a0b93c2a61776ce6d63b2642085c2a03
Armand WADJI <armand_wadji@yahoo.fr>
2022-04-19 11:56:27 +0200

    total quantity

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -9,8 +9,8 @@ import {
 
 const addToCartDOM = () => {
   const addProducts = document.querySelectorAll(".product-cart-btn");
+  const quantity = getElement(".cart-item-count");
   let panier = setStorageItem() ? setStorageItem() : [];
-  console.log(panier);
 
   addProducts.forEach((product) => {
     product.addEventListener("click", async (e) => {
@@ -18,11 +18,18 @@ const addToCartDOM = () => {
       const data = await fetchProducts(`${singleProductUrl}?id=${id}`);
       const { price } = data.fields;
       const { url: img } = data.fields.image[0];
-
-      panier = [...panier, { id, price, img, amount: 1 }];
-      getStorageItem(panier);
+      let foundProduct = panier.find((item) => item.id === id);
+      if (!foundProduct) {
+        panier = [...panier, { id, price, img, amount: 1 }];
+        getStorageItem(panier);
+      } else {
+        panier.map((item) => item.id === id && item.amount++);
+        getStorageItem(panier);
+      }
     });
   });
+  const quantities = panier.reduce((acc, val) => acc + val.amount, 0);
+  quantity.textContent = quantities;
 };
 
 export default addToCartDOM;
