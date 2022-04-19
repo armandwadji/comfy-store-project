4bd8505db9eddd10c2cfa662990d1f80eb98fe35
Armand WADJI <armand_wadji@yahoo.fr>
2022-04-19 15:30:00 +0200

    total price

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ import { setupStore, store } from "./src/store.js";
 import display from "./src/displayProducts.js";
 import { getElement } from "./src/utils.js";
 import { allProductsUrl } from "./src/utils.js";
+import addToCartDOM from "./src/cart/addToCartDOM.js";
 
 const featuredCenter = getElement(".featured-center");
 window.addEventListener("DOMContentLoaded", async () => {
   const data = await fetchProducts(allProductsUrl);
-  data.length = 3;
-  const productsFeatured = display(data);
+  const featured = data.filter((item) => item.fields.featured && item);
+  const productsFeatured = display(featured);
   featuredCenter.innerHTML = productsFeatured;
+  addToCartDOM();
 });
--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -9,18 +9,18 @@ import {
 
 const addToCartDOM = () => {
   const addProducts = document.querySelectorAll(".product-cart-btn");
-  const quantity = getElement(".cart-item-count");
+
   let panier = setStorageItem() ? setStorageItem() : [];
 
   addProducts.forEach((product) => {
     product.addEventListener("click", async (e) => {
       const id = product.dataset.id;
       const data = await fetchProducts(`${singleProductUrl}?id=${id}`);
-      const { price } = data.fields;
+      const { price, name: title } = data.fields;
       const { url: img } = data.fields.image[0];
       let foundProduct = panier.find((item) => item.id === id);
       if (!foundProduct) {
-        panier = [...panier, { id, price, img, amount: 1 }];
+        panier = [...panier, { id, price, img, title, amount: 1 }];
         getStorageItem(panier);
       } else {
         panier.map((item) => item.id === id && item.amount++);
@@ -28,8 +28,41 @@ const addToCartDOM = () => {
       }
     });
   });
+
+  const cartItems = getElement(".cart-items");
+  cartItems.innerHTML = panier
+    .map(({ id, price, img, title, amount }) => {
+      return `
+  <article class="cart-item" data-id=${id}>
+    <img src=${img} alt="photo de ${title}" class="cart-item-img" />
+    <div>
+      <h4 class="cart-item-name">${title}</h4>
+      <p class="cart-item-price">$${price}</p>
+      <button class="cart-item-remove-btn" data-id=${id}>remove</button>
+    </div>
+
+    <div>
+      <button class="cart-item-increase-btn" data-id=${id}>
+        <i class="fas fa-chevron-up"></i>
+      </button>
+      <p class="cart-item-amount" data-id="id">${amount}</p>
+      <button class="cart-item-decrease-btn" data-id=${id}>
+        <i class="fas fa-chevron-down"></i>
+      </button>
+    </div>
+  </article>
+    `;
+    })
+    .join("");
+
+  const quantity = getElement(".cart-item-count");
   const quantities = panier.reduce((acc, val) => acc + val.amount, 0);
   quantity.textContent = quantities;
+
+  const totalDOM = getElement(".cart-total");
+  const total = panier.reduce((acc, val) => acc + val.amount * val.price, 0);
+
+  totalDOM.textContent = `total : $${total}`;
 };
 
 export default addToCartDOM;
